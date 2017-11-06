6aa3f155fef79e95966f606a2844e946079d7549
watsonso <uni0360@gmail.com>
2017-11-06 20:33:23 +0900

    finish checker

--- checker/js/main.js
+++ checker/js/main.js
@@ -1,11 +1,19 @@
 (function() {
   'use strict';
 
-  var comment = document.getElemetById('comment');
-  var lebel = document.getElemetById('label');
+  var comment = document.getElementById('comment');
+  var lebel = document.getElementById('label');
 
   var LIMIT = 20;
+  var WARNING = 10;
 
   label.innerHTML = LIMIT;
+
+  comment.addEventListener('keyup', function() {
+    var remaining = LIMIT - this.value.length;
+    label.innerHTML = remaining;
+    
+    label.className = remaining < WARNING ? 'warning' : '';
+  });
 
 })();
