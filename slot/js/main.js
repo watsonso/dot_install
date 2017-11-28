5df8a74fcfcbe3bedff2a7494b37ffacf9e677b7
watsonso <uni0360@gmail.com>
2017-11-28 09:30:52 +0900

    make main.js

---
+++ slot/js/main.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  var panel = document.getElementsByClassName('panel');
+  var spin = document.getElementById('spin');
+
+  var cards = [
+    'seven.png',
+    'bell.png',
+    'cherry.png'
+  ];
+
+  spin.addEventListener('click', function() {
+
+  });
+})
