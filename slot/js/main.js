93eabe419f7219912eb0a1744e468bfd3df339b5
watsonso <uni0360@gmail.com>
2017-11-29 08:05:36 +0900

    make initPanels

--- slot/js/main.js
+++ slot/js/main.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var panel = document.getElementsByClassName('panel');
+  var panels = document.getElementsByClassName('panel');
   var spin = document.getElementById('spin');
 
   var cards = [
@@ -10,7 +10,34 @@
     'cherry.png'
   ];
 
-  spin.addEventListener('click', function() {
+  var timers = [];
 
+  var stopCount = 0;
+
+  function runSlot(n) {
+    timers[n] = setInterval(function() {
+      panels[n].children[0].src =
+        'img/' +
+        cards[Math.floor(Math.random() * cards.length)];
+        //runSlot(n);
+    }, 50);
+  }
+
+  function initPanel() {
+    var i;
+    for (i = 0; i < panels.length; i++) {
+      panels[i].children[1].addEventListener('click', function() {
+        clearTimeout(timers[this.dataset.index]);
+      });
+    }
+  }
+
+  initPanel();
+
+  spin.addEventListener('click', function() {
+    var i;
+    for (i = 0; i < panels.length; i++) {
+      runSlot(i);
+    }
   });
-})
+})();
