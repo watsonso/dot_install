7571cbfd032026f5a6cb067a10331a68c888c511
watsonso <uni0360@gmail.com>
2017-12-02 14:59:59 +0900

    complete slot

--- slot/js/main.js
+++ slot/js/main.js
@@ -16,7 +16,7 @@
   var stopCount = 0;
 
   function runSlot(n) {
-    timers[n] = setInterval(function() {
+    timers[n] = setTimeout(function() {
       panels[n].children[0].src =
         'img/' +
         cards[Math.floor(Math.random() * cards.length)];
@@ -28,8 +28,35 @@
     var i;
     for (i = 0; i < panels.length; i++) {
       panels[i].children[1].addEventListener('click', function() {
+        if (this.className.indexOf('inactive') !== -1) {
+          return;
+        }
         clearTimeout(timers[this.dataset.index]);
+        stopCount++;
+        this.className = 'stop inactive';
+        if (stopCount === panels.length) {
+          stopCount = 0;
+          checkResult();
+        }
       });
+    }
+  }
+
+  function checkResult() {
+    var img0 = panels[0].children[0];
+    var img1 = panels[1].children[0];
+    var img2 = panels[2].children[0];
+
+    if (img0.src !== img1.src && img0.src !== img2.src) {
+      img0.className = 'unmatched';
+    }
+
+    if (img1.src !== img0.src && img1.src !== img2.src) {
+      img1.className = 'unmatched';
+    }
+
+    if (img2.src !== img0.src && img2.src !== img1.src) {
+      img2.className = 'unmatched';
     }
   }
 
@@ -37,13 +64,15 @@
 
   spin.addEventListener('click', function() {
     var i;
+    if (this.className.indexOf('inactive') !== -1) {
+      return;
+    }
+    this.className = 'inactive';
     for (i = 0; i < panels.length; i++) {
       runSlot(i);
+      panels[i].children[0].className = '';
+      panels[i].children[1].className = 'stop';
     }
   });
 
-  stop.addEventListener('click', function() {
-    stopCount++;
-    console.log(stopCount);
-  });
 })();
